082f7d9b640824c15222cb80a35524299a1614cb
Ethan Guo <ethguo@gmail.com>
2016-11-19 01:00:41 -0500

    Added boilerplate files and barebones flask app.

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+
+import os
+import requests
+
+app = Flask(__name__)
+
+DEVICE_ID = os.environ["DEVICE_ID"]
+ACCESS_TOKEN = os.environ["ACCESS_TOKEN"]
+
+@app.route("/")
+def getIndex():
+	return render_template("index.html")
+
+@app.route("/post", methods=["POST"])
+def post():
+	command = request.form["command"]
+
+	url = "https://api.particle.io/v1/devices/%s/dtmf?access_token=%s" % (DEVICE_ID, ACCESS_TOKEN)
+	data = {"args": command}
+	r = requests.post(url, data=data)
+
+	print(r.text)
+	return r.text
+
+if __name__ == '__main__':
+	app.run(debug=True)
